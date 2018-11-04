f39f146201701b9c272b79c16ed5ec6cbfbf1c93
Kowalski1337 <33070907+Kowalski1337@users.noreply.github.com>
2018-11-04 23:41:05 +0300

    Grammar of expression added

---
+++ Expression.hs
@@ -0,0 +1,31 @@
+module Expressions where
+  import Data.List
+
+  data Binop = Impl | Conj | Disj | Sum | Mul | Equality
+  instance Show Binop where
+    show Impl = "->"
+    show Conj = "&"
+    show Disj = "|"
+    show Sum = "+"
+    show Mul = "*"
+    show Equality = "="
+
+  data Quantifier = Any | Exists
+  instance Show Quantifier where
+    show Any = "@"
+    show Exists = "?"
+
+  data Expression = Binary Binop Expression Expression
+                    | Negation Expression
+                    | Variable String
+                    | Function String [Expression]
+                    | Quant Quantifier Expression Expression
+                    | Next Expression
+
+  instance Show Expression where
+    show (Quant q first second) = "(" ++ show q ++ show first ++ show second ++ ")"
+    show (Binary op first second) = "(" ++ show first ++ show op ++ show second ++ ")"
+    show (Negation expr) = "!" ++ show expr ++ ""
+    show (Variable s) = s
+    show (Next expr) = show expr ++ "\'"
+    show (Function f args) = f ++ "(" ++ (intercalate ", " (map show args)) ++ ")"
